168634614f9f25e3533e0fbdeab80c0cc3d092f0
A1r4s0e3nIY <ushakovarsenidenis@gmail.com>
2025-12-22 19:16:41 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,46 @@
 import arcade
+from arcade.gui import UIManager, UIFlatButton, UITextureButton, UILabel, UIInputText, UITextArea, UISlider, UIDropdown, \
+    UIMessageBox
+from arcade.gui.widgets.layout import UIAnchorLayout, UIBoxLayout
+
+
+class FirstGame(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title, center_window=True)
+        arcade.set_background_color(arcade.color.GRAY)
+        
+        # UIManager — сердце GUI
+        self.manager = UIManager()
+        self.manager.enable()  # Включить, чтоб виджеты работали
+        
+        # Layout для организации — как полки в шкафу
+        self.anchor_layout = UIAnchorLayout()  # Центрирует виджеты
+        self.box_layout = UIBoxLayout(vertical=True, space_between=10)  # Вертикальный стек
+        
+        # Добавим все виджеты в box, потом box в anchor
+        self.setup_widgets()  # Функция ниже
+        
+        self.anchor_layout.add(self.box_layout)  # Box в anchor
+        self.manager.add(self.anchor_layout)  # Всё в manager
+
+    def setup_widgets(self):
+        continue_button = UIFlatButton(text="Продолжить", width=200, height=50, color=arcade.color.BLUE)
+        continue_button.on_click = self.continueBt
+        self.box_layout.add(continue_button)
+
+        exit_button = UIFlatButton(text="Выйти из игры", width=200, height=50, color=arcade.color.BLUE)
+        exit_button.on_click = self.continueBt
+        self.box_layout.add(exit_button)
+
+    def continueBt(self, event):
+        arcade.close_window()
+
+    def exitBt(self, event):
+        arcade.close_window()
+
+    def on_draw(self):
+        self.clear()
+        self.manager.draw()
 
 
 class GridGame(arcade.Window):
@@ -47,7 +89,7 @@ class GridGame(arcade.Window):
         elif key == arcade.key.LEFT:
             self.change_x = -self.speed
         if key == arcade.key.ESCAPE:
-            arcade.close_window()
+            fg = FirstGame(arcade.get_display_size()[0] // 2, arcade.get_display_size()[1] // 2, "Меню")
 
     def on_key_release(self, key, modifiers):
         if key in [arcade.key.LEFT, arcade.key.RIGHT]:
