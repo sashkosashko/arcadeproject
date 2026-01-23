c2812c50a0eb31686c662a44f979487afe34f49f
Иван <vanamelcikov7275@gmail.com>
2026-01-23 08:07:32 +0400

    upd: textures

--- assets/start_scene_tiles2.tsx
+++ assets/start_scene_tiles2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="i" tilewidth="32" tileheight="32" tilecount="105" columns="15">
- <image source="../../../Downloads/i.webp" width="480" height="240"/>
+ <image source="textures/i.webp" width="480" height="240"/>
 </tileset>
